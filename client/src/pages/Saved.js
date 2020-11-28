89b363048505ce6000ae19ef5b2f46736bbf96b0
Francis Albuna <falbuna1@gmail.com>
2020-11-28 15:31:36 -0800

    Added routes for the books api. Added controllers for the routes.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
 function Saved(){
+
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        loadBooks()
+      }, [])
+
+    function loadBooks() {
+        API.getBooks()
+        .then(res => 
+        setBooks(res.data)
+        )
+        .catch(err => console.log(err));
+    };
+    
+
     return(
 
         <div>
             <h1>Saved Books</h1>
-            
+
         </div>
     )
 }
