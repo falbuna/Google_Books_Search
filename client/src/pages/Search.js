c4fb8a937ef0301663004cd47dc1279ba40b8706
Francis Albuna <falbuna1@gmail.com>
2020-11-28 12:59:48 -0800

    Installed tailwind for the application. Got a search bar. Got a nav bar.

---
+++ client/src/pages/Search.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchBar from "../components/SearchBar"
+
+function Search(){
+    return(
+
+        <div>
+            <SearchBar />
+        </div>
+    )
+}
+
+export default Search;
