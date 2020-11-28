b22679861e3edeea4a383511c18f681374dfd89c
Francis Albuna <falbuna1@gmail.com>
2020-11-28 14:40:00 -0800

    Added the models for the book schema. Started added the routes.

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    authors:  [String],
+    description: { type: String },
+    image:  { type: String },
+    link: { type: String },
+    title: { type: String }
+})
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
