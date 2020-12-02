84def9e523564af6ab1bebb20f2eb9eefa71ccf0
Francis Albuna <falbuna1@gmail.com>
2020-12-02 11:33:15 -0800

    Changed the font color in the header.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -8,7 +8,7 @@ function Header(){
     <h1 className="text-3xl font-bold leading-tight text-gray-900">
       (React) Google Books Search
     </h1>
-    <h2 className="text-lg font-bold leading-tight text-blue-900">Search for and Save Books of Interest</h2>
+    <h2 className="text-lg font-bold leading-tight text-blue-700">Search for and Save Books of Interest</h2>
   </div>
 </header>
 
