d9672e1ef438689246119edadcc4adb005638a08
Francis Albuna <falbuna1@gmail.com>
2020-12-02 15:01:59 -0800

    Deploying.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const routes = require("./routes")
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
 
